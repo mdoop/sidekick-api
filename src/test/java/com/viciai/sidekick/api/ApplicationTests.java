b25ccec11962a291864fda35b1bef4dfa10ed7ea
Max <maxdupslaff@live.com>
2023-04-08 16:13:54 -0500

    initial commit

---
+++ src/test/java/com/viciai/sidekick/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viciai.sidekick.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
